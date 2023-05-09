c61c194423163323bc15ddeddf713e3f0810366d
Marian-Bernice <hmarnice@gmail.com>
2023-05-09 06:53:08 +0000

    error fix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,24 +5,27 @@ Rotate 2D Matrix
 
 
 def rotate_2d_matrix(matrix):
-    """rotate two dimension matrix 90 degrees clockwise
-    Args:
-        matrix (list[[list]]): a matrix
+    """
+    Rotate a 2D matrix by 90 degrees (clockwise).
     """
     n = len(matrix)
-    for i in range(int(n / 2)):
-        y = (n - i - 1)
-        for j in range(i, y):
-            x = (n - 1 - j)
-            # current number
-            tmp = matrix[i][j]
-            # change top for left
-            matrix[i][j] = matrix[x][i]
-            # change left for bottom
-            matrix[x][i] = matrix[y][x]
-            # change bottom for right
-            matrix[y][x] = matrix[j][y]
-            # change right for top
-            matrix[j][y] = tmp
+    for layer in range(n // 2):
+        first = layer
+        last = n - 1 - layer
+        for i in range(first, last):
+            offset = i - first
+            top = matrix[first][i]
 
+            # left -> top
+            matrix[first][i] = matrix[last - offset][first]
 
+            # bottom -> left
+            matrix[last - offset][first] = matrix[last][last - offset]
+
+            # right -> bottom
+            matrix[last][last - offset] = matrix[i][last]
+
+            # top -> right
+            matrix[i][last] = top
+
+    return matrix
